bd3e28da13cb8d1143e503846b0bf026cec01800
vikash kumar <vikashkumargupta5a7@gmail.com>
2020-05-04 19:51:56 +0530

    [vikash]add. logger in project

--- src/test/java/com/bridgelabz/testBase/BaseClass.java
+++ src/test/java/com/bridgelabz/testBase/BaseClass.java
@@ -2,11 +2,12 @@
  * 
  */
 package com.bridgelabz.testBase;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class BaseClass {
 
@@ -14,15 +15,22 @@ public class BaseClass {
 	public String username = "vikashkumargupta5a7@gmail.com";
 	public String password = "vikash1";
 	public static WebDriver driver;
+	
+	public static Logger logger;
+	
 	@BeforeClass
 	public void setup() {
+		
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//Drivers//chromedriver.exe");
 		driver = new ChromeDriver();
+		logger = Logger.getLogger("Fundoo Notes");
+		PropertyConfigurator.configure("Log4j.properties");
+	
 	}
-
+	
 	@AfterClass
 	public void tearDown() {
-//		driver.quit();
+		driver.quit();
 	}
 
 }
--- src/test/java/com/bridgelabz/testBase/TC_LoginTest_01.java
+++ src/test/java/com/bridgelabz/testBase/TC_LoginTest_01.java
@@ -1,7 +1,5 @@
 
 package com.bridgelabz.testBase;
-
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,17 +12,22 @@ public class TC_LoginTest_01 extends BaseClass {
 	@Test
 	public void Logintest() {
 		driver.get(baseURL);
-
+		logger.info("Url is opened");
+		
 		LoginPage lp = new LoginPage(driver);
 		lp.serEmailAddress(username);
+		
+		logger.info("Entered email ");
 		lp.setPassword(password);
+		
+		logger.info("Entered password ");
 		lp.clickSubmit();
 		if (driver.getTitle().equals("FundooNotes")) {
-			System.out.println("Title of " + driver.getTitle());
+			logger.info("Login test passed..");
 			Assert.assertTrue(true);
 		}
 		else { 
-			System.out.println("Title of " + driver.getTitle());
+			logger.info("Login test failed..");
 			Assert.assertTrue(false);
 		}
 
